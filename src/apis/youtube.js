1cca2860a62cbf30cfdfa7984bfc41a60fcd1ae0
beatlejooce <44638462+beatlejooce@users.noreply.github.com>
2019-08-25 10:48:17 -0400

    Update youtube.js
    removed api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyB0V4SoeHorbrVkFGs8YngDveGE1qHAuxA";
+const KEY = 
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
